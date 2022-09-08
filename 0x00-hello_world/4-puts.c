e62065f88c8099d7bd888e227b88a98d0b011b2a
Ejenihu_Christopher <ejenihukelechi@gmail.com>
2022-09-08 12:56:51 -0700

    love

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,21 @@
+/**
+ *
+ *  * main -entry point
+ *
+ *   *
+ *
+ *    * Return: 0 after printing the function
+ *
+ *     */
+
 #include <stdio.h>
 
-/**
-* Description: using the main function
-* this program prints "programming is like building a multilingual puzzle
-* Return: 0
-*/
 int main(void)
+
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+
+			puts("\"Programming is like building a multilingual puzzle");
+
+						return (0);
+
 }
