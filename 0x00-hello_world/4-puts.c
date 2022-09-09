7295f1cc34c0f3c4b5bb1ccfe8f83ef8cf6bb210
Ejenihu_Christopher <ejenihukelechi@gmail.com>
2022-09-08 20:15:47 -0700

    er

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 /**
 *main - main block
+*
+*Description using the main function
+*this program prints "Programming is like building a multilingual puzzle
 *Return: 0
 */
 int main(void)
