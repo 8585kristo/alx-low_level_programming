79e4a276b2344bf2b7e0ef9d0291251ec39738b0
Ejenihu_Christopher <ejenihukelechi@gmail.com>
2022-09-08 18:42:57 -0700

    Risk

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-/*
+/**
  *
- *main - entry point
+ *main - main block
  *
  *Return: 0
  *
@@ -12,8 +12,8 @@ int main(void)
 
 {
 
-printf("with proper grammar, but the outcome is a piece of art,\n"); 
-	
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
 
 }
