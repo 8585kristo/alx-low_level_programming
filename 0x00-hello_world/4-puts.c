afe593c20b92d310bedcc1d8975e5863fc161177
Ejenihu_Christopher <ejenihukelechi@gmail.com>
2022-09-08 21:28:22 -0700

    gg

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,10 @@
 *main - main block
 *Description using the main function
 *this program prints "Programming is like building a multilingual puzzle
-*Return: 0
+                    *Return: 0
 */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return (0);
+                return (0);
 }	
