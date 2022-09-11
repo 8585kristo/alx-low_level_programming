c2ff724baff0121fb950c0e2c980e81c883a4f0b
Ejenihu_Christopher <ejenihukelechi@gmail.com>
2022-09-11 04:05:43 -0700

    task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -4,15 +4,15 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (success)
+ * Return: Always 0(Success)
  */
 int main(void)
 {
-int 1;
+	int 1;
 
-for (1 = 0; 1 < 10; 1++)
-	printf("%d", 1);
-printf("\n");
+	for (1 = 0; 1 < 10; 1++)
+		printf("%d", 1);
+	printf("\n");
 
-return (0);
+	return (0);
 }
