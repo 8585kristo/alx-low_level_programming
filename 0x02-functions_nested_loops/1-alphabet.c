e02a7ffa294807e921dc4ea484850108cc1a2fd3
Ejenihu_Christopher <ejenihukelechi@gmail.com>
2022-09-14 06:11:19 -0700

    jjj

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * print_alphabet - afunctions that prints the alphabet, in lowercase,
- * folowed by a new line.
+ * print_alphabet - a functions that prints the alphabet, in lowercase,
+ * followed by a new line.
  *
- * Desscription: print_alphabet will print a-z using putchar
+ * Description: print_alphabet will print a-z using putchar
  * Return: always equals to zero
  */
 
