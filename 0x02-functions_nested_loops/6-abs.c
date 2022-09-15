20fe85287aac03534d9c73ed92974dd0127547cd
Ejenihu_Christopher <ejenihukelechi@gmail.com>
2022-09-14 17:27:01 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_last_digit - a function that prints the last digit of a number
+ * @c: integer whose value is unknown
+ *
+ * Description: a function that prints the last digit of a number
+ * Return: the value of the last digit
+ */
+
+int print_last_digit(int c)
+{
+	int i;
+
+	i = c % 10;
+
+		if (i < 0)
+		i *= -1;
+	_putchar(i + '0');
+
+	return (i);
+}
