7fae4b90aecc2cd7154158730b981bcac08c779a
Ejenihu_Christopher <ejenihukelechi@gmail.com>
2022-10-04 15:17:23 -0700

    task6

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,52 +1,44 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
- * argstostr - concatenates all the arguments of a program.
- * @ac: argument count.
- * @av: argument vector.
- *
- * Return: pointer of an array of char
- */
+ * argstostr - this function concatenates the arguments
+(* a blank line
+ *@ac: args count
+ *@av: args vector
+* Description: this function concatenates the argument)?
+(* section header: the header of this function is holberton.h)*
+* Return: this function return a char pointer to the concatenate arguments.
+*/
 char *argstostr(int ac, char **av)
 {
-	char *aout;
-	int c, i, j, ia;
+	char *p, *q;
+	int i, j, result;
 
-	if (ac == 0)
+	if (ac == 0 || av == NULL)
 		return (NULL);
-
-	for (c = i = 0; i < ac; i++)
+	for (i = 0, result = 0; i < ac; i++, result++)
 	{
-		if (av[i] == NULL)
-			return (NULL);
-
-		for (j = 0; av[i][j] != '\0'; j++)
-			c++;
-		c++;
-	}
-
-	aout = malloc((c + 1) * sizeof(char));
-
-	if (aout == NULL)
-	{
-		free(aout);
-		return (NULL);
-	}
-
-	for (i = j = ia = 0; ia < c; j++, ia++)
-	{
-		if (av[i][j] == '\0')
+		j = 0;
+		while (*(*(av + i) + j) != '\0')
 		{
-			aout[ia] = '\n';
-			i++;
-			ia++;
-			j = 0;
+			j++;
+			result++;
 		}
-		if (ia < c - 1)
-			aout[ia] = av[i][j];
 	}
-	aout[ia] = '\0';
-
-	return (aout);
-
+	result++;
+	p = malloc(result * sizeof(char));
+	if (p == NULL)
+		return (NULL);
+	q = p;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			*p = av[i][j];
+			p++;
+		}
+		*p = '\n';
+		p++;
+	}
+	return (q);
+}
